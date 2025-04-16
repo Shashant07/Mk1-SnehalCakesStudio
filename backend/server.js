931c5470be1279670cb50147d29ebd990d41d9b2
shashant07 <shashantpandit@gmail.com>
2025-04-17 00:50:09 +0530

    express5 issues fixed

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('/*', (req, res) =>
+  app.get('/{*splat}', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
